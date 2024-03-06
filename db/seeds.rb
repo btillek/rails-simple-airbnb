eb5d409489b7e42753c2f3bc9b3c8ab932812000
boy tillekens <boytillekens@gmail.com>
2024-03-06 16:48:14 +0100

    create the index action and cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,33 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.delete_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Dark space in Berlin',
+  address: '10 Paul Lincke Ufer 11000',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 60,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Light space in Neukolln',
+  address: '244 Hermannplatz 11900',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 65,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Dump in Kreuzberg',
+  address: '244 Gorlitzerplatz 11900',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 65,
+  number_of_guests: 4
+)
